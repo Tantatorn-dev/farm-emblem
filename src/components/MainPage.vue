34d1be344fcbab71a18b6e2512fb3290abfcd953
Tantatorn <timefear007@gmail.com>
2019-09-04 16:50:55 +0700

    random allies position

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -58,44 +58,64 @@ export default {
 
   created() {
     // random enemies position
-    let countEnemies = 0;
-    while (countEnemies != 2) {
+    let count = 0;
+    while (count != 2) {
       let randRow = Math.floor(Math.random() * 4);
       let randCol = Math.floor(Math.random() * 7);
-      if (this.tableData[randRow][randCol] != "b") {
+      if (this.tableData[randRow][randCol] != "") {
         continue;
       } else {
         this.tableData[randRow][randCol] = "L";
-        countEnemies++;
+        count++;
       }
     }
 
-    countEnemies = 0;
-    while (countEnemies != 2) {
+    count = 0;
+    while (count != 2) {
       let randRow = Math.floor(Math.random() * 4);
       let randCol = Math.floor(Math.random() * 7);
       if (this.tableData[randRow][randCol] != "") {
         continue;
       } else {
         this.tableData[randRow][randCol] = "S";
-        countEnemies++;
+        count++;
       }
     }
 
-    countEnemies = 0;
-    while (countEnemies != 2) {
+    count = 0;
+    while (count != 2) {
       let randRow = Math.floor(Math.random() * 4);
       let randCol = Math.floor(Math.random() * 7);
       if (this.tableData[randRow][randCol] != "") {
         continue;
       } else {
         this.tableData[randRow][randCol] = "A";
-        countEnemies++;
+        count++;
       }
     }
 
     //random allies position
-    countEnemies = 0;
+    count = 0;
+    while (count != 3) {
+      let randRow = Math.floor(Math.random() * 3) + 5;
+      let randCol = Math.floor(Math.random() * 7);
+      if (this.tableData[randRow][randCol] != "") {
+        continue;
+      } else {
+        switch (count) {
+          case 0:
+            this.tableData[randRow][randCol] = "L";
+            break;
+          case 1:
+            this.tableData[randRow][randCol] = "S";
+            break;
+          case 2:
+            this.tableData[randRow][randCol] = "A";
+            break;
+        }
+        count++;
+      }
+    }
   },
 
   methods: {
