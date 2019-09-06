9f65cfee91f69661f5c05fc2eed1617a7e690489
Tantatorn <timefear007@gmail.com>
2019-09-06 23:39:40 +0700

    create preloadCharacter function

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -41,6 +41,8 @@
 </template>
 
 <script>
+import { preloadCharacter } from "../lib/preload";
+
 export default {
   name: "MainPage",
 
@@ -63,65 +65,7 @@ export default {
   },
 
   created() {
-    // random enemies position
-    let count = 0;
-    while (count != 2) {
-      let randRow = Math.floor(Math.random() * 4);
-      let randCol = Math.floor(Math.random() * 7);
-      if (this.tableData[randRow][randCol] != "") {
-        continue;
-      } else {
-        this.tableData[randRow][randCol] = "l";
-        count++;
-      }
-    }
-
-    count = 0;
-    while (count != 2) {
-      let randRow = Math.floor(Math.random() * 4);
-      let randCol = Math.floor(Math.random() * 7);
-      if (this.tableData[randRow][randCol] != "") {
-        continue;
-      } else {
-        this.tableData[randRow][randCol] = "s";
-        count++;
-      }
-    }
-
-    count = 0;
-    while (count != 2) {
-      let randRow = Math.floor(Math.random() * 4);
-      let randCol = Math.floor(Math.random() * 7);
-      if (this.tableData[randRow][randCol] != "") {
-        continue;
-      } else {
-        this.tableData[randRow][randCol] = "a";
-        count++;
-      }
-    }
-
-    //random allies position
-    count = 0;
-    while (count != 3) {
-      let randRow = Math.floor(Math.random() * 3) + 5;
-      let randCol = Math.floor(Math.random() * 7);
-      if (this.tableData[randRow][randCol] != "") {
-        continue;
-      } else {
-        switch (count) {
-          case 0:
-            this.tableData[randRow][randCol] = "L";
-            break;
-          case 1:
-            this.tableData[randRow][randCol] = "S";
-            break;
-          case 2:
-            this.tableData[randRow][randCol] = "A";
-            break;
-        }
-        count++;
-      }
-    }
+    preloadCharacter(this.tableData);
   },
 
   methods: {
---
+++ src/lib/preload.js
@@ -0,0 +1,61 @@
+export function preloadCharacter(tableData) {
+    // random enemies position
+    let count = 0;
+    while (count != 2) {
+        let randRow = Math.floor(Math.random() * 4);
+        let randCol = Math.floor(Math.random() * 7);
+        if (tableData[randRow][randCol] != "") {
+            continue;
+        } else {
+            tableData[randRow][randCol] = "l";
+            count++;
+        }
+    }
+
+    count = 0;
+    while (count != 2) {
+        let randRow = Math.floor(Math.random() * 4);
+        let randCol = Math.floor(Math.random() * 7);
+        if (tableData[randRow][randCol] != "") {
+            continue;
+        } else {
+            tableData[randRow][randCol] = "s";
+            count++;
+        }
+    }
+
+    count = 0;
+    while (count != 2) {
+        let randRow = Math.floor(Math.random() * 4);
+        let randCol = Math.floor(Math.random() * 7);
+        if (tableData[randRow][randCol] != "") {
+            continue;
+        } else {
+            tableData[randRow][randCol] = "a";
+            count++;
+        }
+    }
+
+    //random allies position
+    count = 0;
+    while (count != 3) {
+        let randRow = Math.floor(Math.random() * 3) + 5;
+        let randCol = Math.floor(Math.random() * 7);
+        if (tableData[randRow][randCol] != "") {
+            continue;
+        } else {
+            switch (count) {
+                case 0:
+                    tableData[randRow][randCol] = "L";
+                    break;
+                case 1:
+                    tableData[randRow][randCol] = "S";
+                    break;
+                case 2:
+                    tableData[randRow][randCol] = "A";
+                    break;
+            }
+            count++;
+        }
+    }
+}
